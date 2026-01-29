2da931c7620eeca62a54d40ac27acb9bdf27a1a6
Souhaib <massroursouhaib@hotmail.com>
2026-01-29 10:21:20 +0100

    change header and commit for merge with dev

--- django_app_prediction/settings.py
+++ django_app_prediction/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Static global du projet
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
