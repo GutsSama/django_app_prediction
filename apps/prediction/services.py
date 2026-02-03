343595aa399abd4afb5139831a2b98d19e5f0a05
Fatima <you@example.com>
2026-02-03 16:36:37 +0100

    feat: make all the functions for model in the file service

--- apps/prediction/services.py
+++ apps/prediction/services.py
@@ -1,22 +1,49 @@
-import joblib
 import os
+import joblib
+import pandas as pd
 from django.conf import settings
 
-
 MODEL_PATH = os.path.join(
-    settings.BASE_DIR, 
-    'apps', 
-    'prediction', 
-    'ml_models', 
+    settings.BASE_DIR,
+    'apps',
+    'prediction',
+    'ml_models',
     'insurance_model.joblib'
 )
 
 
+_MODEL = None
+
+
 def load_model():
     """
-    Charge et retourne le modèle ML.
+    Charge le modèle ML UNE SEULE FOIS
     """
-    
-    if os.path.exists(MODEL_PATH):
-        return joblib.load(MODEL_PATH)
-    return None
+    global _MODEL
+
+    if _MODEL is None:
+        if not os.path.exists(MODEL_PATH):
+            raise FileNotFoundError("Le modèle ML est introuvable")
+
+        _MODEL = joblib.load(MODEL_PATH)
+
+    return _MODEL
+
+
+def predict(data, bmi):
+    """
+    Fait la prédiction ML
+    """
+    model = load_model()
+
+    input_data = pd.DataFrame([{
+        'age': data['age'],
+        'sex': data['sex'],
+        'bmi': bmi,
+        'children': data['children'],
+        'region': data['region'],
+        'smoker': data['is_fumeur'],
+    }])
+
+    prediction = model.predict(input_data)[0]
+    return round(float(prediction), 2)
--- apps/prediction/views.py
+++ apps/prediction/views.py
@@ -3,20 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
 from .forms import PredictionForm
-import joblib
-import os
-import pandas as pd
-from .services import load_model
-
-
-
+from .services import predict
 from django.conf import settings
 from apps.accounts.models import AccountUser
-
-
-
-
-MODEL_PATH = os.path.join(settings.BASE_DIR, 'apps', 'prediction', 'ml_models', 'insurance_model.joblib')
 
 
 class PredictionView(LoginRequiredMixin, FormView):
@@ -48,21 +37,13 @@ class PredictionView(LoginRequiredMixin, FormView):
 
 
     def form_valid(self, form):
-        # Charger le modèle
-        self.model = load_model()
-        
-        if self.model is None:
-            messages.error(self.request, "Le modèle n'a pas été trouvé.")
-            return self.render_to_response(self.get_context_data(form=form))
-
 
         data = form.cleaned_data
         taille = data['taille'] / 100
         bmi = data['poids'] / (taille ** 2)
 
-
         try:
-            prediction = self.predict(data, bmi)
+            prediction = predict(data, bmi)
             messages.success(self.request, f'Charges estimées : {prediction:.2f} €')
             self.prediction_result = prediction
 
@@ -78,20 +59,3 @@ class PredictionView(LoginRequiredMixin, FormView):
         if hasattr(self, 'prediction_result'):
             context['prediction'] = self.prediction_result
         return context
-
-
-    def predict(self, data, bmi):
-        input_data = pd.DataFrame([{
-            'age': data['age'],
-            'sex': data['sex'],
-            'bmi': bmi,
-            'children': data['children'],
-            'region': data['region'],
-            'smoker' : data['is_fumeur']
-        }])
-
-
-        # Utiliser le modèle chargé
-        prediction = self.model.predict(input_data)[0]
-        return round(prediction, 2)
-
