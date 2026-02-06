6683af2c4be206458c9e25b4764cb62adeb7c05f
Souhaib Massrour <massroursouhaib@hotmail.com>
2026-02-06 09:42:24 +0100

    fix/appointments

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,10 +1,38 @@
 // static_src/tailwind.config.js
 module.exports = {
   content: [
-    '../templates/**/*.html',           // templates à la racine
-    '../../templates/**/*.html',        // templates dans l'app
-    '../../**/templates/**/*.html',     // tous les templates
+    './templates/**/*.html',           // templates à la racine
+    './../templates/**/*.html',        // templates dans l'app
+    './../**/templates/**/*.html',     // tous les templates
     './src/**/*.js',                    // fichiers JS dans src
+  ],
+    safelist: [
+    'bg-red-500',
+    'text-white',
+    'p-4',
+    'table-auto',
+    'border-2',
+    'border-black',
+    'min-w-full',
+    'divide-y',
+    'divide-gray-200',
+    'bg-gray-50',
+    'px-6',
+    'py-3',
+    'text-left',
+    'text-xs',
+    'font-medium',
+    'text-gray-500',
+    'uppercase',
+    'tracking-wider',
+    'bg-white',
+    'whitespace-nowrap',
+    'text-sm',
+    'text-gray-900',
+    'hover:bg-gray-50',
+    'px-3',
+    'py-1',
+    'text-xs',
   ],
   theme: {
     extend: {
@@ -28,7 +56,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('daisyui'),
+    //require('daisyui'),
   ],
   daisyui: {
     themes: ["light"], 
